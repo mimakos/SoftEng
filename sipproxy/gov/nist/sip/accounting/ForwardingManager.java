6c34f2d1e0554b2e15811c523bd599bf7e171709
Dimitris Konomis <dim.konomis@gmail.com>
2013-05-31 13:58:42 +0300

    ForwardingManager fixed

--- sipproxy/gov/nist/sip/accounting/ForwardingManager.java
+++ sipproxy/gov/nist/sip/accounting/ForwardingManager.java
@@ -1,6 +1,7 @@
 package gov.nist.sip.accounting;
 
 import java.sql.*;
+import java.util.*;
 
 public class ForwardingManager {
 	private static ForwardingManager singleton = null;
@@ -12,62 +13,78 @@ public class ForwardingManager {
 		return singleton;
 	}
 	
-	public boolean isForwarded(int forwarder, int forwardee) {
+	public boolean isForwarding(int user) {
         String selectQuery = "SELECT\n"
                            + "    forwarder\n"
                            + "FROM\n"
                            + "    forward\n"
                            + "WHERE\n"
-                           + "    forwarder = ?\n"
-                           + "    AND forwardee = ?\n";
+                           + "    forwarder = ?\n";
+
         try {
-        	PreparedStatement preparedStatement = Database.getConnection().prepareStatement(selectQuery);
-        	preparedStatement.setInt(1, forwarder);
-	        preparedStatement.setInt(2, forwardee);
-	        ResultSet rs = preparedStatement.executeQuery();
+        	PreparedStatement pstmt = Database.getConnection().prepareStatement(selectQuery);
+       	    pstmt.setInt(1, user);
+	        ResultSet rs = pstmt.executeQuery();
 	        return rs.next();
         }
-        catch (SQLException e) {
-        	e.printStackTrace();
+        catch (SQLException sqlE) {
+        	sqlE.printStackTrace();
         	return false;
         }
 	}
 	
-	public boolean forward(int forwarder, int forwardee){
+	public int getForwardee(int user) {
+		
+		int forwardee = -1;
+		
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String selectQuery = " SELECT\n"
+                           + "      forwardee\n"
+                           + "FROM\n"
+                           + "      forward\n"
+                           + "WHERE\n"
+                           + "forwarder = ?\n";
+		
 		try {
-			String insertQuery = "INSERT INTO\n"
-			                   + "	   forward\n"
-			                   + "(forwarder, forwardee)\n"
-			                   + "VALUES\n"
-			                   + "(?, ?)\n";
-			PreparedStatement pstmt = Database.getConnection().prepareStatement(insertQuery);
-			pstmt.setInt(1,  forwarder);
-			pstmt.setInt(2,  forwardee);
-			pstmt.executeUpdate();
-			return true;
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-			return false;
+			pstmt = Database.getConnection().prepareStatement(selectQuery);
+			pstmt.setInt(1, user);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				forwardee = rs.getInt(2);
+			}
+			return forwardee;
+		} catch (SQLException sqlE) {
+			sqlE.printStackTrace();
+			return (-1);
 		}
 	}
 	
-	public boolean unforward(int forwarder, int forwardee) {
-		try {
-			String deleteQuery = "DELETE FROM\n"
-		                       + "    forward\n"
-		                       + "WHERE\n"
-		                       + "    forwarder = ?\n"
-		                       + "    AND forwardee = ?\n";
-			PreparedStatement pstmt = Database.getConnection().prepareStatement(deleteQuery);
-			pstmt.setInt(1, forwarder);
-			pstmt.setInt(2, forwardee);
-			pstmt.executeUpdate();
-			return true;
-		}
-		catch (SQLException e) {
-			return false;
+	public int getFinalForwardee(int caller, int callee) {
+		
+		int forwardee = 0; 
+		int finalForwardee = 0;
+		
+		Set<Integer> s = new HashSet<Integer>();
+		s.add(caller);
+		s.add(callee);
+		
+		forwardee = getForwardee(callee);
+		finalForwardee = forwardee;
+		while ( forwardee != -1 ) {	
+			finalForwardee = forwardee; 
 			
+			if (s.contains(forwardee)) {
+				// loop detected in forwarding
+				forwardee = -1;
+				finalForwardee = forwardee;
+			}
+			else {
+				s.add(forwardee);
+				forwardee = getForwardee(forwardee);
+			}	
 		}
-    }
+		return finalForwardee;	
+	}
 }
